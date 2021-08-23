a71d530c4673ba36ae3344eb7a2bb3b3697da0ae
coffmanjrp <coffmanjrp@gmail.com>
2021-08-22 17:10:51 -0700

    Implement click effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [isClicked, setIsClicked] = useState(false);
+  const [left, setLeft] = useState('');
+  const [top, setTop] = useState('');
+
+  const handleClick = (e) => {
+    setIsClicked(true);
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    const buttonTop = e.target.offsetTop;
+    const buttonLeft = e.target.offsetLeft;
+
+    const xInside = x - buttonLeft;
+    const yInside = y - buttonTop;
+
+    setLeft(xInside);
+    setTop(yInside);
+
+    setTimeout(() => setIsClicked(false), 500);
+  };
+
   return (
     <div className="App">
-      <button className="ripple">Click Me</button>
+      <button type="button" className="ripple" onClick={(e) => handleClick(e)}>
+        Click Me
+        {isClicked && <span className="circle" style={{ top, left }} />}
+      </button>
     </div>
   );
 }
